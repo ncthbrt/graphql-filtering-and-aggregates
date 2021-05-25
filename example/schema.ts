5fa40cfcc5c8002781f88eedc7f5292b2b61fc35
Natalie Cuthbert <natalie@stitch.money>
2021-05-25 20:56:15 +0200

    Defining API

--- example/schema.ts
+++ example/schema.ts
@@ -3,18 +3,21 @@ import fs from 'fs';
 import path from 'path';
 import resolvers from './resolvers';
 import { SchemaComposer } from 'graphql-compose';
-import { injectDateFormattingMiddleware } from '../lib/dateformatting-middleware';
+import { addInjectAggregateDirective, injectAggregatesMiddleware } from '../lib/aggregate-middleware';
 
 const schemaText = fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8');
+
+const composer = new SchemaComposer();
+
+addInjectAggregateDirective(composer);
 
 const unenrichedSchema = makeExecutableSchema({
     typeDefs: gql`${schemaText}`,
     resolvers: resolvers()
 });
 
-const composer = new SchemaComposer(unenrichedSchema);
-
-injectDateFormattingMiddleware(composer);
+composer.merge(unenrichedSchema);
+injectAggregatesMiddleware(composer);
 
 
 const builtSchema = composer.buildSchema();
